1f2c1bc0376a6cfef083ec0eeefaef6bd2e6c71f
701alisherjan <701alisherjan@gmail.com>
2025-08-08 18:52:48 +0500

    finish

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
       <div className="container mx-auto">
         <div className="w-full border-b border-gray-700 pb-6">
           <div className="flex flex-col md:flex-row md:gap-16 items-start md:items-start">
-            <div className="flex items-center space-x-3 mb-6 md:mb-0 mt-10 ml-[360px] ">
+            <div className="flex items-center space-x-3 mb-6 md:mb-0 mt-10 ml-[260px] ">
               <FaCircleUser className="text-4xl text-green-400" />
               <span className="text-2xl font-bold text-green-400">
                 IT Center
@@ -26,12 +26,26 @@ const Footer = () => {
               <h3 className="font-medium text-xl mb-2 text-amber-50">
                 Biz haqimizda
               </h3>
-              <p className="text-sm text-gray-300">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Necessitatibus in odio perspiciatis deleniti similique quo atque
-                quia quae corrupti ullam modi vero veritatis repellendus, quod
-                aut! Quia corporis natus atqu.
-              </p>
+              <div>
+                <a className="text-cyan-500 gap-2" href="#">
+                  <span className="mr-2">Email pochtamiz :</span>
+                  <span className="text-white">teamsoftware@gmail.com</span>
+                </a>
+                <h3 className="">
+                  <span className="text-blue-400">Asoschi :</span> Timur Ahmatov
+                </h3>
+                <h3 className="">
+                  <span className="text-green-600">Manzil :</span>
+                  <span className="ml-2">
+                    Jizzax viloyati Zarbdor tumani Adliya bo`limi 1 qavati
+                    binosi
+                  </span>
+                </h3>
+              </div>
+              <div className="flex gap-2">
+                <h3 className="text-red-500">Telefon raqam :</h3>
+                <span>+998 77 030 4321</span>
+              </div>
             </div>
 
             <div className="ml-[50px]">
@@ -40,42 +54,42 @@ const Footer = () => {
               </h4>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 text-base">
                 <a
-                  href="#"
+                  href="https://telegram.com/"
                   className="flex items-center gap-2 hover:text-blue-400 transition duration-300"
                 >
                   <FaTelegramPlane />
                   Telegram
                 </a>
                 <a
-                  href="#"
+                  href="https://instagram.com"
                   className="flex items-center gap-2 hover:text-pink-500 transition duration-300"
                 >
                   <FaInstagram />
                   Instagram
                 </a>
                 <a
-                  href="#"
+                  href="https://github.com/AhmatovT"
                   className="flex items-center gap-2 hover:text-gray-300 transition duration-300"
                 >
                   <FaGithub />
                   GitHub
                 </a>
                 <a
-                  href="#"
+                  href="https://twitter.com"
                   className="flex items-center gap-2 hover:text-blue-300 transition duration-300"
                 >
                   <FaTwitter />
                   Twitter
                 </a>
                 <a
-                  href="#"
+                  href="https://whatsapp.com"
                   className="flex items-center gap-2 hover:text-green-400 transition duration-300"
                 >
                   <FaWhatsapp />
                   WhatsApp
                 </a>
                 <a
-                  href="#"
+                  href="https://youtube.com"
                   className="flex items-center gap-2 hover:text-red-600 transition duration-300"
                 >
                   <FaYoutube />
